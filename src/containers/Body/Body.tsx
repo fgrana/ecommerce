10de7d68137b90102756e25b4ecf5e36377afc89
fgrana <federicorgrana@gmail.com>
2024-09-10 22:36:29 -0300

    Supousedly now works?

--- src/containers/Body/Body.tsx
+++ src/containers/Body/Body.tsx
@@ -1,22 +1,23 @@
 import CardList from "../../components/card/Cards";
 import { CardClass } from "../../util/Card";
 
-export default function Body(){
-
+export default function Body() {
   const charizardCard = new CardClass("Charizard", 15, 15, 15, 15);
-const ListC = [[charizardCard,charizardCard,charizardCard,charizardCard],
-[charizardCard,charizardCard,charizardCard,charizardCard],
-[charizardCard,charizardCard,charizardCard,charizardCard],
-[charizardCard,charizardCard,charizardCard,charizardCard]]
+  const SquirtleCard = new CardClass("Squirtle", 15, 15, 15, 15);
+  const ListC = [
+    [charizardCard, SquirtleCard, charizardCard, charizardCard],
+    [charizardCard, charizardCard, SquirtleCard, charizardCard],
+    [charizardCard, charizardCard, charizardCard, charizardCard],
+    [charizardCard, charizardCard, SquirtleCard, SquirtleCard],
+  ];
 
   const pokemonList = ListC.map((crd) => {
-    return <CardList {...crd} />;})
+    return <CardList {...crd} />;
+  });
 
-  return( <table>
-    <tbody>
-        {pokemonList}
-    </tbody>
-  </table> );
-
-
+  return (
+    <table>
+      <tbody>{pokemonList}</tbody>
+    </table>
+  );
 }
