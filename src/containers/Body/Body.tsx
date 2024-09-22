f505d0867944d688fcfd42d02991dc32b7d9446b
fgrana <federicorgrana@gmail.com>
2024-09-22 03:08:49 -0300

    Finally Get to call the api

--- src/containers/Body/Body.tsx
+++ src/containers/Body/Body.tsx
@@ -1,10 +1,66 @@
+import { useEffect, useState } from "react";
 import CardList from "../../components/card/Cards";
 import { Pagination } from "../../components/pagination/Pagination";
 import { CardClass } from "../../util/Card";
 import body from "./body.module.css";
 
+const BASE_URL = 'https://pokeapi.co/api/v2/'
+
+
+export interface Root {
+  id: number
+  name: string
+  stats: Stat[]
+}
+
+export interface Stat {
+  base_stat: number
+  effort: number
+  stat: Stat2
+}
+//---------------------------------------------------
+
+
+export interface pokemonList{
+  post: Post
+}
+
+export interface Post {
+  name: string
+  stats: Stat[]
+}
+
+export interface Stat {
+  base_stat: number
+  effort: number
+  stat: Stat2
+}
+
+export interface Stat2 {
+  name: string
+  url: string
+}
+
 export default function Body() {
+  const [page, setPage] = useState(0);
+  const [posts, setPosts] = useState<Post>();
+  useEffect(() => {
+    const fetchPosts = async () => {
+        const response = await fetch(`${BASE_URL}pokemon/ditto`,{
+          method: "GET",
+          headers: {
+            "Access-Control-Allow-Origin" : "*",
+            "Content-type": "application/json; charset=UTF-8"
+          , mode:'cors'}});
+        const posts = (await response.json()) as Post;
+        setPosts(posts);
+    };
+
+    fetchPosts();
+  }, [page]);
+
   
+  console.log(posts?.name)
   const charizardCard = new CardClass("Charizard", 534, 78, 84, 78, 109, 85, 100);
   const SquirtleCard = new CardClass("Squirtle", 534, 78, 84, 78, 109, 85, 100);
   const ListC = [
